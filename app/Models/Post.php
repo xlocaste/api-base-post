925fc2d4c4fbf792d0a268e207ab5f5050e70a27
xlocaste <irfandi.di122@gmail.com>
2024-11-02 11:30:44 +0700

    start project

---
+++ app/Models/Post.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    protected $table = 'posts';
+
+    protected $fillable = [
+        'image',
+        'title',
+        'content',
+    ];
+}
